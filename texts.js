f33c386f5ee2a4cfd8d40cd13f16f38eb246cc50
ricardovanlaarhoven <ricardo1994@gmail.com>
2018-03-07 16:07:14 +0100

    this fixes built errors

--- texts.js
+++ texts.js
@@ -1,10 +1,8 @@
-let texts = {
+export default {
     save: 'Save',
     from: 'from',
     till: 'till',
     'no-data': 'There is nothing found',
     'no-results': 'There is nothing found for this filter',
     'rows-per-page-text': 'Rows per page',
-
 };
-export default texts;
