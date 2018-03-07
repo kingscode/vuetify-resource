3139395088010025e996e53302adfbe7a85b3ea8
ricardovanlaarhoven <ricardo1994@gmail.com>
2018-03-07 15:15:05 +0100

    added text/translation support fixes #9

---
+++ texts.js
@@ -0,0 +1,10 @@
+let texts = {
+    save: 'Save',
+    from: 'from',
+    till: 'till',
+    'no-data': 'There is nothing found',
+    'no-results': 'There is nothing found for this filter',
+    'rows-per-page-text': 'Rows per page',
+
+};
+export default texts;
