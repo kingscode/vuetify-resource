1fc1b6b1039ea42cf9a78fd0680692b1494f026e
ricardovanlaarhoven <ricardo1994@gmail.com>
2018-07-12 08:57:00 +0200

    first try for the ActivityOverlay

--- VuetifyResource.vue
+++ VuetifyResource.vue
@@ -22,8 +22,8 @@
                     </v-toolbar>
                     <v-card-text>
                         <slot name="createContent"></slot>
+                        <activity-overlay/>
                     </v-card-text>
-                    <div style="flex: 1 1 auto;"></div>
                 </v-card>
             </v-dialog>
 
@@ -48,8 +48,8 @@
                     </v-toolbar>
                     <v-card-text>
                         <slot name="updateContent"></slot>
+                        <activity-overlay/>
                     </v-card-text>
-                    <div style="flex: 1 1 auto;"></div>
                 </v-card>
             </v-dialog>
 
@@ -174,9 +174,11 @@
 
 <script>
     import texts from './texts.js';
+    import ActivityOverlay from './components/ActivityOverlay';
 
     export default {
         name: 'ResourceList',
+        components: {ActivityOverlay},
         data() {
             return {
                 meta: {
@@ -650,6 +652,27 @@
 </script>
 
 <style>
+    .indicator-overlay-wrapper .overlay{
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: #000000;
+        opacity: 0.2;
+        z-index: 99;
+        pointer-events: bounding-box;
+    }
+
+    .indicator-overlay-wrapper .indicator-wrapper{
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        margin-left: -35px;
+        margin-top: -35px;
+        z-index: 100;
+    }
+
     .vuetify-resource {
         position: relative;
         margin-top: 30px;
---
+++ components/ActivityOverlay.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="indicator-overlay-wrapper" v-if="value">
+        <div class="overlay"></div>
+        <div class="indicator-wrapper">
+            <v-progress-circular
+                :size="70"
+                :width="7"
+                color="white"
+                indeterminate
+            ></v-progress-circular>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'activity-overlay',
+        props: {
+            value: {
+                default: false,
+                required: true,
+                type: Boolean
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
