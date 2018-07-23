e2942717a95ed1efdc215d31a38673ebe921fded
ricardovanlaarhoven <ricardo1994@gmail.com>
2018-07-23 14:16:34 +0200

    replaced css

--- components/ActivityOverlay.vue
+++ components/ActivityOverlay.vue
@@ -27,5 +27,24 @@
 </script>
 
 <style scoped>
+    .indicator-overlay-wrapper .overlay{
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: #000000;
+        opacity: 0.4;
+        z-index: 99;
+        pointer-events: bounding-box;
+    }
 
+    .indicator-overlay-wrapper .indicator-wrapper{
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        margin-left: -35px;
+        margin-top: -35px;
+        z-index: 100;
+    }
 </style>
