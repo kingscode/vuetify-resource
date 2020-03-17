895fb366c388ee85829021f4c70884f86f62e5de
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-03-17 12:49:45 +0100

    remove vuetify loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 import VuetifyResourceComponent from './VuetifyResource.vue';
 
+Vue.use(Vuetify);
 const VuetifyResource = {
     install(Vue, options) {
         Vue.component('vuetify-resource', VuetifyResourceComponent);
